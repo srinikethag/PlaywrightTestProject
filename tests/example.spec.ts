e4bce1330ff73bb8c7afdcb7d7375aaf3314a587
srinikethag <sriniketha.geejagar@toggl.com>
2023-02-22 19:38:56 +0530

    added example test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,10 @@
+// example.spec.ts
 import { test, expect } from '@playwright/test';
+import { homePage } from '../library/PlaywrightLibraryTest/pageObjects/homePage';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+test('Open Installation page', async ({ page }) => {
+  const homepage = new homePage(page);
+  await homepage.goto();
+  await homepage.getStarted();
+  await expect(homepage.installPlaywright).toHaveText("Installing Playwright");
 });
